46f280e3dcfff7c7cbfbde564f58b7cb4e2381d6
Jordan Bourque <Jordan@Jordans-MacBook-Pro.local>
2016-03-05 15:28:08 -0500

    cleaned up the code, added explanations

--- challange2.cpp
+++ challange2.cpp
@@ -4,9 +4,7 @@
 using namespace std;
 int main()
 {
-	int testNum = 0, hold = 0;
-	int xint = 0, yint = 0;
-	bool prime = true;
+	int testNum = 0, hold = 0, xint = 0, yint = 0;
 	cin >> testNum;
 	while (testNum > 0)
 	{
@@ -15,23 +13,23 @@ int main()
 		{
 			while (true)
 			{
-				if (xint <= 1)
+				if (xint <= 1) // since 1, 0 and any negative numbers are not prime
 				{
 					break;
 				}
-				else if (xint <= 3)
+				else if (xint <= 3) // since 2 and 3 are prime
 				{
 					cout << xint << endl;
 					break;
 				}
-				if (xint % 2 == 0 || xint % 3 == 0)
-				{
+				else if (xint % 2 == 0 || xint % 3 == 0) //note: if 2 and 3 are not set to primes before this
+				{										 //it will filter out 2 and 3 saying they are not prime
 					break;
 				}
-				else
+				else //This else statement is checking for primes using the trial division primality test
 				{
 					hold = sqrt(xint) + 1;
-					if (hold % 2 == 0)
+					if (hold % 2 == 0) //Since primes cannot be even, this makes hold odd
 					{
 						hold++;
 					}
@@ -41,11 +39,11 @@ int main()
 						{
 							hold = 0;
 						}
-						hold = hold-2;
+						hold = hold-2; //Since hold cannot be even and be prime, we simply skip all even numbers
 					}
-					if (hold > 0)
+					if (hold > 0) //If the while function ends without xint % hold == 0, than we know it is prime
 					{
-						cout << xint << endl;
+						cout << xint << endl; 
 					}
 					break;
 				}
@@ -57,5 +55,3 @@ int main()
 	}
 	return 0;
 }
-//100 000 000 0
-// 99 900 000 0
