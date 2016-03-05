9971400f7b8e8f7d72beaeaad226a33c7264f6e9
BourqueJ <bourque2@mail.usf.edu>
2016-03-05 15:09:38 -0500

    Update challange1.cpp

--- challange1.cpp
+++ challange1.cpp
@@ -5,7 +5,7 @@ int main()
 	int num = 0;
 	while (num != 42)
 	{
-		cin >> num;
+		cin >> num; // cin terminates after num == 42, so no infinite loop occurs.
 		if (num != 42)
 			cout << num << endl;
 		else
